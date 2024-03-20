70e48e695d989be7e2a4558e8a0240d460d538b3
Glitch (glitch-hello-react) <none>
2024-03-20 23:57:05 +0000

    ✨🐦 Checkpoint ./src/pages/home.jsx:66085921/7848

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,10 +4,7 @@ import { useWiggle } from "../hooks/wiggle";
 import { Link } from "wouter";
 
 // Our language strings for the header
-const strings = [
-  "Bahasa Dekolonial",
-  "Decolonial Language",
-];
+const strings = ["Bahasa Dekolonial", "Decolonial Language"];
 
 // Utility function to choose a random value from the language array
 function randomLanguage() {
@@ -15,18 +12,18 @@ function randomLanguage() {
 }
 
 /**
-* The Home function defines the content that makes up the main content of the Home page
-*
-* This component is attached to the /about path in router.jsx
-* The function in app.jsx defines the page wrapper that this appears in along with the footer
-*/
+ * The Home function defines the content that makes up the main content of the Home page
+ *
+ * This component is attached to the /about path in router.jsx
+ * The function in app.jsx defines the page wrapper that this appears in along with the footer
+ */
 
 export default function Home() {
   /* We use state to set the hello string from the array https://reactjs.org/docs/hooks-state.html
      - We'll call setHello when the user clicks to change the string
   */
   const [hello, setHello] = React.useState(strings[0]);
-  
+
   /* The wiggle function defined in /hooks/wiggle.jsx returns the style effect and trigger function
      - We can attach this to events on elements in the page and apply the resulting style
   */
@@ -34,10 +31,9 @@ export default function Home() {
 
   // When the user clicks we change the header language
   const handleChangeHello = () => {
-    
     // Choose a new Hello from our languages
     const newHello = randomLanguage();
-    
+
     // Call the function to set the state string in our component
     setHello(newHello);
   };
@@ -48,11 +44,25 @@ export default function Home() {
       <div className="grid-container">
         <div className="paragraph1">
           <h2>The Waroeng Experience</h2>
-          <p>Waroeng in Indonesia have started to be embodied by “cheap” fonts, such as impact, strechted type, cooper black and more. The use of these fonts are not the same in a western context, they are more used or they used to be more used for more conventional stuff. Waroeng have embraced new meanings and new purposes for the font, they are now used more unconventionally, in bold store fronts fulll of color and often strechted to the max.</p>
+          <p>
+            Waroeng in Indonesia have started to be embodied by “cheap” fonts,
+            such as impact, strechted type, cooper black and more. The use of
+            these fonts are not the same in a western context, they are more
+            used or they used to be more used for more conventional stuff.
+            Waroeng have embraced new meanings and new purposes for the font,
+            they are now used more unconventionally, in bold store fronts fulll
+            of color and often strechted to the max.
+            
+            The graphic identity of Warung in Indonesia should be celebrated and embraced rather than seen as cheap. The practicality and the commonalities have emerged to give it its own charm.
+          </p>
         </div>
         <div className="paragraph2">
           <h2>Typography as a Program</h2>
-          <p>Latinisation - latin bias, Unicode standardisation and showcasing alternatives, materiality of type, holistic thinking, preserving scripts</p>
+          <p>
+            Latinisation - latin bias, Unicode standardisation and showcasing
+            alternatives, materiality of type, holistic thinking, preserving
+            scripts, taking back the public space / public signage
+          </p>
         </div>
         <div className="paragraph3">
           <h2>Questions of Neutrality</h2>
