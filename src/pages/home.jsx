c907b5069770d103a5f3871130025e9d601efabb
Glitch (glitch-hello-react) <none>
2024-05-02 10:23:59 +0000

    🏞🌕 Checkpoint ./src/pages/home.jsx:66085921/1597 ./src/styles/styles.css:66085921/345

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -39,11 +39,7 @@ export default function Home() {
   };
   return (
     <>
-      <h1 className="title">{hello}</h1>
-      {/* When the user hovers over the image we apply the wiggle style to it */}
-      <h1 className="title">
-        >>> Decolonial Design <br></br>CAN TYPEFACES CHANGE POSITIONALITY WHEN BEING USED VERNACULARLY?
-      </h1>
+      
       <div className="grid-container">
         <div className="paragraph1">
           <div className="scroll-container">
@@ -313,7 +309,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-
+      
       <div className="about-section">
         <div className="about-project">
           <h2>
@@ -355,6 +351,11 @@ export default function Home() {
           </p>
         </div>
       </div>
+      <h1 className="title">{hello}</h1>
+      {/* When the user hovers over the image we apply the wiggle style to it */}
+      <h1 className="title">
+        >>> Decolonial Design <br></br>CAN TYPEFACES CHANGE POSITIONALITY WHEN BEING USED VERNACULARLY?
+      </h1>
     </>
   );
 }
